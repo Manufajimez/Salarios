a14cb622f16c0326dcb33e8ce93f99ce40d9ab4e
Manufajimez <146924006+Manufajimez@users.noreply.github.com>
2024-01-24 12:07:45 +0100

    k

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.8
 RUN pip install pandas scikit-learn streamlit
 COPY src/app.py /app/
-COPY model/Salary_model.pkl /app/model/Salary_model.pkl 
+COPY model/salary_model.pkl /app/model/salary_model.pkl 
 WORKDIR /app
 ENTRYPOINT [ "streamlit", "run", "app.py"]
--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import joblib
 
-clf = joblib.load('data/Salary_model.pkl')
+clf = joblib.load('model/salary_model.pkl')
 st.title('Calcula el salario!')
 
 job_title = st.number_input('job_title:', value=0, step=1)  # Asegurando que sea un número entero
@@ -10,10 +10,9 @@ experience_level = st.number_input('experience_level:', value=0, step=1)  # Aseg
 company_location = st.number_input('company_location:', value=0, step=1)  # Asegurando que sea un número entero
 company_size = st.number_input('company_size:', value=0, step=1)  # Asegurando que sea un número entero
 
-# Quitar esta línea ya que no parece necesaria
-# salary = st.selectbox('Predice tu salario', [])
 
 if st.button('Calcular'):
-    X = pd.DataFrame([[job_title, experience_level, company_location, company_size]], columns=["job title", "experience level", "company location", "company size"])
-    prediction = clf.predict(X)[0]
-    st.text(f"El salario predicho es: {prediction}")
+
+   X=[[job_title, experience_level, company_location, company_size]]
+   prediction = clf.predict(X)
+   st.text(f"El salario predicho es: {prediction}")
