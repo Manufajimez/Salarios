cd53561b02945ccf1336e9e9baf37a6a268ba312
Manufajimez <146924006+Manufajimez@users.noreply.github.com>
2024-01-24 10:59:16 +0100

    6

--- src/app.py
+++ src/app.py
@@ -5,16 +5,15 @@ import joblib
 clf = joblib.load('data/Salary_model.pkl')
 st.title('Calcula el salario!')
 
-experience_level = st.number_input('Height:')
 job_title = st.number_input('job_title:')
+experience_level = st.number_input('experience_level:')
 company_location = st.number_input('company_location:')
 company_size = st.number_input('company_size:')
 
-salary = st.selectbox(
-    'Predice tu salario',  
-)
+# Quitar esta línea ya que no parece necesaria
+# salary = st.selectbox('Predice tu salario', [])
 
 if st.button('Submit'):
-    X = pd.DataFrame([[experience_level, job_title, experience_level, company_location, company_size]], columns=["Experience level", "job title", "experience level", "company location", "company size"])
+    X = pd.DataFrame([[job_title, experience_level, company_location, company_size]], columns=["job title", "experience level", "company location", "company size"])
     prediction = clf.predict(X)[0]
-    st.text(f"This pet is a {prediction}")
+    st.text(f"El salario predicho es: {prediction}")
