21f7a75828f7f187200e90501dba3471a5e258b8
Manufajimez <146924006+Manufajimez@users.noreply.github.com>
2024-01-25 09:21:59 +0100

    H

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.8
-RUN pip install pandas scikit-learn streamlit
+RUN pip install pandas scikit-learn streamlit 
+RUN pip install joblib==1.1.0
 COPY src/app.py /app/
-COPY model/salary_model.pkl /app/model/salary_model.pkl 
+COPY model/salary_model2.pkl /app/model/salary_model2.pkl 
 WORKDIR /app
 ENTRYPOINT [ "streamlit", "run", "app.py"]
--- src/app.py
+++ src/app.py
@@ -2,17 +2,45 @@ import streamlit as st
 import pandas as pd 
 import joblib
 
-clf = joblib.load('model/salary_model.pkl')
+# Asegúrate de que 'clf' sea realmente tu modelo y no un DataFrame
+#clf = joblib.load('model/salary_model2.pkl')
+
+
+# Cargar datos desde el CSV
+data = pd.read_csv('\data\salarios_nuevo.csv')
+
 st.title('Calcula el salario!')
 
-job_title = st.number_input('job_title:', value=0, step=1)  # Asegurando que sea un número entero
-experience_level = st.number_input('experience_level:', value=0, step=1)  # Asegurando que sea un número entero
-company_location = st.number_input('company_location:', value=0, step=1)  # Asegurando que sea un número entero
-company_size = st.number_input('company_size:', value=0, step=1)  # Asegurando que sea un número entero
+# Mapeo para 'job_title'
+job_title_mapping = dict(zip(data['job_title'].unique(), data['job_title_label'].unique()))
+
+# Mapeo para 'experience_level'
+experience_level_mapping = dict(zip(data['experience_level'].unique(), data['experience_level_label'].unique()))
+
+# Mapeo para 'company_location'
+company_location_mapping = dict(zip(data['company_location'].unique(), data['company_location_label'].unique()))
+
+# Mapeo para 'company_size'
+company_size_mapping = dict(zip(data['company_size'].unique(), data['company_size_label'].unique()))
 
 
-if st.button('Calcular'):
+st.title('Calcula el salario!')
 
-   X=[[job_title, experience_level, company_location, company_size]]
-   prediction = clf.predict(X)
-   st.text(f"El salario predicho es: {prediction}")
+job_title = st.selectbox('Job Title:', list(job_title_mapping.keys()))
+experience_level = st.selectbox('Experience Level:', list(experience_level_mapping.keys()))
+company_location = st.selectbox('Company Location:', list(company_location_mapping.keys()))
+company_size = st.selectbox('Company Size:', list(company_size_mapping.keys()))
+
+
+job_title_int = next(key for key, value in job_title_mapping.items() if value == job_title)
+experience_level_int = next(key for key, value in experience_level_mapping.items() if value == experience_level)
+company_location_int = next(key for key, value in company_location_mapping.items() if value == company_location)
+company_size_int = next(key for key, value in company_size_mapping.items() if value == company_size)
+
+# Realizar la predicción
+X = [[job_title_int, experience_level_int, company_location_int, company_size_int]]
+prediction = clf.predict(X)
+st.text(f"El salario predicho es: {prediction}")
+
+
+
